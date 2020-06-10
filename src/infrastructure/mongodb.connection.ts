40518e349cd75286bff993155073b86b3b1625d3
clement <cmrtzlff@gmail.com>
2020-06-10 17:50:38 +0200

    fix: rename mongodb.connection

--- src/infrastructure/mongodb.connection.ts
+++ src/infrastructure/mongodb.connection.ts
@@ -1,18 +1,11 @@
 import { connect } from 'mongoose';
 
-async function main(): Promise<void> {
-  try {
-    await connect(
-      'mongodb+srv://dbAdmin:qwe@cluster0-mkbwe.mongodb.net/twitter?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log('connection ok !');
-  } catch (error) {
-    console.log(error);
+connect(
+  'mongodb+srv://dbAdmin:qwe@cluster0-mkbwe.mongodb.net/twitter?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
-}
+);
 
-main();
+console.log('connection ok !');
