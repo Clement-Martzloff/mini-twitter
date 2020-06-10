504feb920dfd3f7d702d056e47e5e7fe403e5755
clement <cmrtzlff@gmail.com>
2020-06-10 17:49:07 +0200

    feat: add session.config

---
+++ src/config/session.config.ts
@@ -0,0 +1,20 @@
+import { app } from '../../app';
+import expressSession from 'express-session';
+import connectMongo from 'connect-mongo';
+const mongoStoreFactory = connectMongo(expressSession);
+import { connection } from 'mongoose';
+
+app.use(
+  expressSession({
+    secret: 'secret',
+    resave: false,
+    cookie: {
+      httpOnly: false,
+      maxAge: 1000 * 60 * 60 * 24 * 14,
+    },
+    store: new mongoStoreFactory({
+      mongooseConnection: connection,
+      ttl: 60 * 60 * 24 * 14,
+    }),
+  })
+);
