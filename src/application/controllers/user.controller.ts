bf9f0e1501fa627384a8f7f039aa5756390643f0
clement <cmrtzlff@gmail.com>
2020-06-10 17:47:51 +0200

    feat: add user.controller

---
+++ src/application/controllers/user.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const signUpForm = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export const signUp = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
