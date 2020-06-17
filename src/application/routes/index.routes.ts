31faf7398025e4e668eb2dd7983489fecf435cb3
clement <cmrtzlff@gmail.com>
2020-06-17 12:33:16 +0200

    chore: rename all.routes to index.routes

--- src/application/routes/index.routes.ts
+++ src/application/routes/index.routes.ts
@@ -1,10 +1,14 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { router as tweetRouter } from './tweet.routes';
 import { router as userRouter } from './user.routes';
+import { router as authRouter } from './auth.routes';
 
 const router = Router();
 
+router.get('/', (_: Request, res: Response) => res.redirect('/tweets'));
+
 router.use('/tweets', tweetRouter);
 router.use('/users', userRouter);
+router.use('/auth', authRouter);
 
 export { router };
