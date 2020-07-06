0e60148dd8925a689703fade909ae12ba8268a3b
clement <cmrtzlff@gmail.com>
2020-07-06 16:23:17 +0200

    feat: username and email of user are unique

--- src/infrastructure/mongoose.models/mongoose.user.model.ts
+++ src/infrastructure/mongoose.models/mongoose.user.model.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from 'mongoose';
 
 const mongooseUserschema = {
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   local: {
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
 };
