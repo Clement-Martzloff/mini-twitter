fbf8a45a55810dd2520c946311b333a323ad15bf
clement <cmrtzlff@gmail.com>
2020-07-06 16:22:04 +0200

    feat: user must be authenticated to access tweets

--- src/application/routes/index.routes.ts
+++ src/application/routes/index.routes.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import { ensureAuthenticated } from '../../config/guards';
 import { router as tweetRouter } from './tweet.routes';
 import { router as userRouter } from './user.routes';
 import { router as authRouter } from './auth.routes';
@@ -7,7 +8,7 @@ const router = Router();
 
 router.get('/', (_: Request, res: Response) => res.redirect('/tweets'));
 
-router.use('/tweets', tweetRouter);
+router.use('/tweets', ensureAuthenticated, tweetRouter);
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
 
---
+++ src/config/guards.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const ensureAuthenticated = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/signin/form');
+  }
+};
