37a6b313914ec0aeec5fd52427450699a041050b
clement <cmrtzlff@gmail.com>
2020-07-08 14:42:57 +0200

    fix: do not use deprecated ensureIndex fn.

--- src/infrastructure/mongodb.connection.ts
+++ src/infrastructure/mongodb.connection.ts
@@ -4,6 +4,7 @@ const uri: string = process.env.MONGO_CONNECTION_URI!;
 connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 console.log('connection ok !');
