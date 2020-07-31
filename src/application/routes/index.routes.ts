5b4911e6baf52d7e9c0201960b47b7df7e272a19
Clement-Martzloff <cmrtzlff@gmail.com>
2020-07-31 16:46:41 +0200

    feat: redirect http to https for all routes

--- src/application/routes/index.routes.ts
+++ src/application/routes/index.routes.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express';
-import { ensureAuthenticated } from '../../config/guards';
+import { ensureSecure, ensureAuthenticated } from '../../config/guards';
 import { router as tweetRouter } from './tweet.routes';
 import { router as userRouter } from './user.routes';
 import { router as authRouter } from './auth.routes';
 
 const router = Router();
 
+router.get('*', ensureSecure);
 router.get('/', (_: Request, res: Response) => res.redirect('/tweets'));
 
 router.use('/tweets', ensureAuthenticated, tweetRouter);
--- src/config/guards.ts
+++ src/config/guards.ts
@@ -3,11 +3,25 @@ import { Request, Response, NextFunction } from 'express';
 export const ensureAuthenticated = (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   if (req.isAuthenticated()) {
     next();
   } else {
     res.redirect('/auth/signin/form');
   }
+};
+
+export const ensureSecure = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (req.secure) {
+    // OK, continue
+    return next();
+  }
+  // handle port numbers if you need non defaults
+  // res.redirect('https://' + req.host + req.url); // express 3.x
+  res.redirect('https://' + req.hostname + req.url); // express 4.x
 };
