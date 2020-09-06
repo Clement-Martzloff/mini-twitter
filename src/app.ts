72a728b93c027f420fc7d0b383cbae664807d105
clement <cmrtzlff@gmail.com>
2020-09-06 12:31:48 +0200

    fix: use correct public path

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(morgan('short'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
